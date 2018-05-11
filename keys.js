a4fc390a835a61c88abbda87639dead63080bc05
Admin <mercerbe@gmail.com>
2018-05-11 09:23:24 -0400

    keys

---
+++ keys.js
@@ -0,0 +1,6 @@
+exports.twitterKeys = {
+  consumer_key: 'yN2KoMEu65aBFGl6IICgX98KS',
+  consumer_secret: 'C20QqMoavq6co5cxgXNBM42hvwLCGoeUylfSFApU2NseQJi88H',
+  acces_token_key: '993590503169052679-zKlkHnD8ZMS7la3Lkeq8x6rEowhKmk4',
+  access_token_secret: 'LXXLAikcPb9bxmp3A0UDt8JYQvZ7XQuws3kgdU4PSlMui',
+}
