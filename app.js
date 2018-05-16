27a50efd6e5485809e5fc04f21ca6c3ede98b882
Admin <mercerbe@gmail.com>
2018-05-16 10:26:34 -0400

    working on default outputs

--- app.js
+++ app.js
@@ -38,7 +38,6 @@ switch (cmd) {
     tweets();
     break;
   case "spotify":
-    //if(x) {spotifySong(x);};
     spotifySong();
     break;
   case "omdb":
@@ -53,7 +52,7 @@ switch (cmd) {
       "1. 'tweets' " + '\n' +
       "2. 'spotify' followed by any song title (e.g. node app spotify 'rocket man') " + '\n' +
       "3. 'omdb' followed by any movie title (e.g. node app omdb Robocop) " + '\n' +
-      "4. 'do' displays random facts " + '\n' +
+      "4. 'do' displays random song " + '\n' +
       "REMINDER: any song or movie title with more than one word needs to be in quotes!");
 };
 
@@ -65,12 +64,12 @@ function tweets() {
   let params = {
     screen_name: userName
   };
-  client.get('statuses/user_timeline', params, (error,tweets, response) => {
+  client.get('statuses/user_timeline', params, (error, tweets, response) => {
     if (!error) {
       for (var i = 0; i < tweets.length; i++) {
         let date = tweets[i].created_at;
         let tweet = "@ben_codes: " + tweets[i].text + "created at: " + date.substring(0, 19);
-        let space = "==========" + i + "==========";
+        let space = "==========" + i + "==========" + '\n';
         console.log(tweet);
         console.log(space);
         fs.appendFile('log.txt', tweet);
@@ -85,68 +84,81 @@ function tweets() {
 
 //spotify
 function spotifySong() {
-  //var song = process.argv.splice(2, process.argv.length - 1);
-  var song = process.argv[3];
-  spotify.search({
-    type: 'track',
-    query: song,
-    limit: 5
-  }, (err, data) => {
-    if (song === '""') {
-      let song = "The Sign";
-    }
-    if (err) {
-      console.log("Error: " + err);
-    } else {
-      for (var i = 0; i < data.tracks.items.length; i++) {
-        let results = data.tracks.items[i];
-        let spotifyResults =
-          "Song: " + results.name + '\n' +
-          "Artist: " + results.artists[0].name + '\n' +
-          "Album: " + results.album.name + '\n' +
-          "Track Number: " + results.track_number + '\n' +
-          "Release Date: " + results.album.release_date + '\n' +
-          "==========" + i + "==========" + '\n';
-        console.log(spotifyResults);
-        fs.appendFile('log.txt', spotifyResults);
-      }
-    }
-  });
-};
-
-//omdb
-function omdb(movies) {
-  let movie = process.argv[3];
-  if (movie === "") {
-    process.argv[3] = "'Mr. Nobody'";
-  }
-  var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&tomatoes=true&apikey=trilogy';
-  request(omdbURL, function(error, response, body) {
-    if (!error && response.statusCode === 200) {
-      var body = JSON.parse(body);
-      //console.log(body);
-      for (var i = 0; i < 1; i++) {
-        if (movie === "Mr. Nobody") {
-          let results = "If you haven't watched 'Mr. Nobody', then you should: http://www.imdb.com/title/tt0485947/ " + '\n' +
-            "It's on Netflix!" + '\n' +
-            "=====================";
-          console.log(results);
-        } else {
-          let results = "Title: " + body.Title + '\n' +
-            "Release Year: " + body.Year + '\n' +
-            "Plot: " + body.Plot + '\n' +
-            "Country of Production: " + body.Country + '\n' +
-            "Language: " + body.Language + '\n' +
-            "Imbd Rating: " + body.imdbRating + '\n' +
-            "Rotten Tomatoes Rating: " + body.Ratings[1] + '\n' +
-            "URL: " + body.tomatoURL + '\n' +
-            "==========" + i + "===========";
-          console.log(results);
-          fs.appendFile('log.txt', results);
+  let song = process.argv[3];
+  if (typeof song === 'undefined') {
+    spotify.request('https://api.spotify.com/v1/tracks/0hrBpAOgrt8RXigk83LLNE').then(function(data) {
+      let results = data;
+      let spotifyResults =
+        "Song: " + results.name + '\n' +
+        "Artist: " + results.artists[0].name + '\n' +
+        "Album: " + results.album.name + '\n' +
+        "Track Number: " + results.track_number + '\n' +
+        "Release Date: " + results.album.release_date + '\n' +
+        "====================" + '\n';
+      console.log(spotifyResults);
+      fs.appendFile('log.txt', spotifyResults);
+    });
+  } else {
+    spotify.search({
+      type: 'track',
+      query: song,
+      limit: 5
+    }, (err, data) => {
+      if (err) {
+        console.log("Error: " + err);
+      } else {
+        for (var i = 0; i < data.tracks.items.length; i++) {
+          let results = data.tracks.items[i];
+          let spotifyResults =
+            "Song: " + results.name + '\n' +
+            "Artist: " + results.artists[0].name + '\n' +
+            "Album: " + results.album.name + '\n' +
+            "Track Number: " + results.track_number + '\n' +
+            "Release Date: " + results.album.release_date + '\n' +
+            "==========" + i + "==========" + '\n';
+          console.log(spotifyResults);
+          fs.appendFile('log.txt', spotifyResults);
         }
       }
-    }
-  });
+    });
+  }
+}
+
+//omdb
+function omdb() {
+  var movie = process.argv[3];
+  if (typeof movie === 'undefined') {
+    let movie = "'Mr. Nobody'";
+    console.log(movie);
+    var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&tomatoes=true&apikey=trilogy';
+    request(omdbURL, function(error, response, body) {
+      if (!error && response.statusCode === 200) {
+        var body = JSON.parse(body);
+        //console.log(body);
+        for (var i = 0; i < 1; i++) {
+          if (movie === "'Mr. Nobody'") {
+            let results = "If you haven't watched 'Mr. Nobody', then you should: http://www.imdb.com/title/tt0485947/ " + '\n' +
+              "It's on Netflix!" + '\n' +
+              "=====================" + '\n';
+            fs.appendFile('log.txt', results);
+            console.log(results);
+          } else {
+            let results = "Title: " + body.Title + '\n' +
+              "Release Year: " + body.Year + '\n' +
+              "Plot: " + body.Plot + '\n' +
+              "Country of Production: " + body.Country + '\n' +
+              "Language: " + body.Language + '\n' +
+              "Imbd Rating: " + body.imdbRating + '\n' +
+              "Rotten Tomatoes Rating: " + body.Ratings[1] + '\n' +
+              "URL: " + body.tomatoURL + '\n' +
+              "==========" + i + "===========" + '\n';
+            console.log(results);
+            fs.appendFile('log.txt', results);
+          }
+        }
+      }
+    });
+  }
 }
 
 //doit --readFile
