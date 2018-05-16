5d0ae719e557754fae1f0f9d2fd00fdd25b56b17
Admin <mercerbe@gmail.com>
2018-05-16 11:29:25 -0400

    add terminal to html

--- app.js
+++ app.js
@@ -1,26 +1,3 @@
-//server
-// const hostname = '127.0.0.1';
-// const port = 3000;
-// const fs = require('fs');
-// const http = require('http');
-// fs.readFile('index.html', (err, html) => {
-//   if (err) {
-//     throw err;
-//   }
-//   const server = http.createServer((req, res) => {
-//     res.statusCode = 200;
-//     res.setHeader('Content-type', 'text/plain');
-//     res.write(html);
-//     res.end();
-//
-//   });
-//
-//   server.listen(port, hostname, () => {
-//     console.log('Server started on port ' + port);
-//   });
-//
-// });
-
 //node modules
 require("dotenv").config();
 const fs = require('fs');
