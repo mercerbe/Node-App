d897c78a918b8bdf7da788d3f5a4cc809bb5ddf1
Admin <mercerbe@gmail.com>
2018-05-16 09:22:13 -0400

    spotify call working to log.txt

--- app.js
+++ app.js
@@ -32,25 +32,13 @@ var spotify = new Spotify(keys.spotify);
 const client = new Twitter(keys.twitterKeys);
 const cmd = process.argv[2];
 
-//stored arg array
-var nodeArgv = process.argv;
-var x = "";
-//multiple words
-for(var i=3; i<nodeArgv.length; i++) {
-  if(i>3 && i<nodeArgv.length){
-    x = x + "+" + nodeArgv[i];
-  }else {
-    x = x + nodeArgv[i];
-  }
-}
-
 //possible cmds
 switch (cmd) {
   case "tweets":
     tweets();
     break;
   case "spotify":
-  //if(x) {spotifySong();} else{ spotifySong("The Sign");}
+    //if(x) {spotifySong(x);};
     spotifySong();
     break;
   case "omdb":
@@ -61,12 +49,12 @@ switch (cmd) {
     break;
     //instructions
   default:
-  console.log("Use the following comands after entering 'node app': " + '\n' +
-                "1. 'tweets' " + '\n' +
-                "2. 'spotify' followed by any song title (e.g. node app spotify 'rocket man') " + '\n' +
-                "3. 'omdb' followed by any movie title (e.g. node app omdb Robocop) " + '\n' +
-                "4. 'do' displays random facts " + '\n' +
-                "REMINDER: any song or movie title with more than one word needs to be in quotes!");
+    console.log("Use the following comands after entering 'node app': " + '\n' +
+      "1. 'tweets' " + '\n' +
+      "2. 'spotify' followed by any song title (e.g. node app spotify 'rocket man') " + '\n' +
+      "3. 'omdb' followed by any movie title (e.g. node app omdb Robocop) " + '\n' +
+      "4. 'do' displays random facts " + '\n' +
+      "REMINDER: any song or movie title with more than one word needs to be in quotes!");
 };
 
 //functions
@@ -74,12 +62,14 @@ switch (cmd) {
 //tweets
 function tweets() {
   let userName = 'ben_codes';
-  let params = {screen_name: userName};
-  client.get('statuses/user_timeline', params, (error, tweets, response) =>  {
-    if(!error) {
+  let params = {
+    screen_name: userName
+  };
+  client.get('statuses/user_timeline', params, (error,tweets, response) => {
+    if (!error) {
       for (var i = 0; i < tweets.length; i++) {
         let date = tweets[i].created_at;
-        let tweet = "@ben_codes: " + tweets[i].text + "created at: " + date.substring(0,19);
+        let tweet = "@ben_codes: " + tweets[i].text + "created at: " + date.substring(0, 19);
         let space = "==========" + i + "==========";
         console.log(tweet);
         console.log(space);
@@ -87,7 +77,7 @@ function tweets() {
         fs.appendFile('log.txt', space);
       }
     } else {
-      console.log("error: " + JSON.stringify(error, null, 2));
+      console.log("error: " + JSON.stringify(error, null, 1));
       return;
     }
   });
@@ -95,57 +85,73 @@ function tweets() {
 
 //spotify
 function spotifySong() {
+  //var song = process.argv.splice(2, process.argv.length - 1);
   var song = process.argv[3];
-  spotify.search({type: 'track', query: song, limit: 1}, (err, data) => {
-      if (song === "") {
-        let song = "The Sign";
-      }
-    if(err) {
+  spotify.search({
+    type: 'track',
+    query: song,
+    limit: 5
+  }, (err, data) => {
+    if (song === '""') {
+      let song = "The Sign";
+    }
+    if (err) {
       console.log("Error: " + err);
     } else {
-      console.log(JSON.stringify(data, null, 2));
-      for (var i = 0; i < data.length; i++) {
-      let results = data.tracks.items[i];
-          var spotifyResults =
-          "Artist: " + results[i].album.artists.name + '\n' + 
-          // "Song: " + results[i].name + '\n' +
-          // "Album: " + results[i].album.name + '\n' +
-          // "Preview: " + results[i].preview_url + '\n' +
-          // "==========" + i + "==========" + '\n';
-          console.log(spotifyResults);
+      for (var i = 0; i < data.tracks.items.length; i++) {
+        let results = data.tracks.items[i];
+        let spotifyResults =
+          "Song: " + results.name + '\n' +
+          "Artist: " + results.artists[0].name + '\n' +
+          "Album: " + results.album.name + '\n' +
+          "Track Number: " + results.track_number + '\n' +
+          "Release Date: " + results.album.release_date + '\n' +
+          "==========" + i + "==========" + '\n';
+        console.log(spotifyResults);
+        fs.appendFile('log.txt', spotifyResults);
       }
     }
   });
 };
 
 //omdb
-function omdb() {
+function omdb(movies) {
   let movie = process.argv[3];
-  var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&tomatoes=true&apikey=trilogy';
-
-  request(omdbURL, function(error, response, body) {
-    if(!error && response.statusCode === 200) {
-      var body = JSON.parse(body);
-      for (var i = 0; i < 1; i++) {
-      let results = "Title: " + body.Title + '\n' +
-                  "Release Year: " + body.Year + '\n' +
-                  "Plot: " + body.Plot + '\n' +
-                  "Country of Production: " + body.Country + '\n' +
-                  "Language: " + body.Language + '\n' +
-                  "Imbd Rating: " + body.imdbRating + '\n' +
-                  "Rotten Tomatoes Rating: " + body.Ratings[1][0]+ '\n' +
-                  "URL: " + body.tomatoURL + '\n' +
-                  "==========" + i + "===========";
-      console.log(results);
-      fs.appendFile('log.txt', results);
-    }
+  if (movie === "") {
+    process.argv[3] = "'Mr. Nobody'";
   }
-});
+  var omdbURL = 'http://www.omdbapi.com/?t=' + movie + '&y=&plot=short&tomatoes=true&apikey=trilogy';
+  request(omdbURL, function(error, response, body) {
+    if (!error && response.statusCode === 200) {
+      var body = JSON.parse(body);
+      //console.log(body);
+      for (var i = 0; i < 1; i++) {
+        if (movie === "Mr. Nobody") {
+          let results = "If you haven't watched 'Mr. Nobody', then you should: http://www.imdb.com/title/tt0485947/ " + '\n' +
+            "It's on Netflix!" + '\n' +
+            "=====================";
+          console.log(results);
+        } else {
+          let results = "Title: " + body.Title + '\n' +
+            "Release Year: " + body.Year + '\n' +
+            "Plot: " + body.Plot + '\n' +
+            "Country of Production: " + body.Country + '\n' +
+            "Language: " + body.Language + '\n' +
+            "Imbd Rating: " + body.imdbRating + '\n' +
+            "Rotten Tomatoes Rating: " + body.Ratings[1] + '\n' +
+            "URL: " + body.tomatoURL + '\n' +
+            "==========" + i + "===========";
+          console.log(results);
+          fs.appendFile('log.txt', results);
+        }
+      }
+    }
+  });
 }
 
 //doit --readFile
 function doIt() {
-  fs.readFile('random.txt', 'utf8', function(error, data){
+  fs.readFile('random.txt', 'utf8', function(error, data) {
     if (!error) {
       let random = data.split(",");
       spotifySong(random[1]);
